fbe10a1043040439ad0f6cb93462ac9d20b905b0
andemar <andemartinez98@hotmail.com>
2019-01-04 17:03:45 -0500

    Prueba de rama UI

---
+++ app/src/main/java/mobi/usc/guiausc/UI2.java
@@ -0,0 +1,18 @@
+package mobi.usc.guiausc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UI2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui2);
+    }
+
+    public void pruebaUI2(){
+        //Esta es la prueba de branchUI2
+    }
+
+}
