9c72c6a30cdb07130d6846f01f2bf63a9d69843c
andemar <andemartinez98@hotmail.com>
2019-01-04 16:08:09 -0500

    Initial Commit

---
+++ app/src/main/java/mobi/usc/guiausc/guiaUsc.java
@@ -0,0 +1,13 @@
+package mobi.usc.guiausc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class guiaUsc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guia_usc);
+    }
+}
