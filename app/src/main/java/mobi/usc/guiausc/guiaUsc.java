a7313b6ca260395e5c50a46235c77cf989d9e5e7
andemar <andemartinez98@hotmail.com>
2019-01-04 19:16:41 -0500

    Creacion pantalla Calendario (Sin codigo) Y Intent de home a calendario.

--- app/src/main/java/mobi/usc/guiausc/guiaUsc.java
+++ app/src/main/java/mobi/usc/guiausc/guiaUsc.java
@@ -1,7 +1,10 @@
 package mobi.usc.guiausc;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.MenuItem;
+        import android.view.View;
 
 public class guiaUsc extends AppCompatActivity {
 
@@ -10,4 +13,37 @@ public class guiaUsc extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guia_usc);
     }
+
+
+
+
+
+
+
+
+
+
+
+    //------------------- METODOS PARA CAMBIAR DE ACTIVITY -----------------------//
+
+    //Metodo que redirige al usuario dependiendo el boton oprimido
+    public void intentHomeCalendario(View view){
+        Intent acHomeCale = new Intent(this, calendario.class);
+        startActivity(acHomeCale);
+    }
+
+    //------------------- PARTE GRAFICA -----------------------//
+
+
+    /**public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.btCalendario){
+            Intent i = new Intent(this, calendario.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
