173a0ef530f9726654c5540beeabefba11e05592
andemar <andemartinez98@hotmail.com>
2019-01-06 14:23:06 -0500

    Creacion pantalla evento (Sin codigo)

---
+++ app/src/main/java/mobi/usc/guiausc/evento.java
@@ -0,0 +1,13 @@
+package mobi.usc.guiausc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class evento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evento);
+    }
+}
