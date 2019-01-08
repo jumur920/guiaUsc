030b40d32032373ca44745a518237ff269ddedc2
andemar <andemartinez98@hotmail.com>
2019-01-08 17:04:41 -0500

    Creacion de cuadro de dialogo "Buscar evento" (Sin codigo).

--- app/src/main/java/mobi/usc/guiausc/guiaUsc.java
+++ app/src/main/java/mobi/usc/guiausc/guiaUsc.java
@@ -1,10 +1,16 @@
 package mobi.usc.guiausc;
 
+        import android.content.DialogInterface;
         import android.content.Intent;
+        import android.support.v7.app.AlertDialog;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.text.Layout;
+        import android.view.LayoutInflater;
         import android.view.MenuItem;
         import android.view.View;
+        import android.widget.Button;
+        import android.widget.Toast;
 
 public class guiaUsc extends AppCompatActivity {
 
@@ -18,19 +24,49 @@ public class guiaUsc extends AppCompatActivity {
 
 
 
+    //-------------------- METODO ONCLICK PARA BOTONES ------------------------//
 
+    public void onClickGuiaUsc(View view){
+      switch (view.getId()){
+          case R.id.btnCalendario:
 
+              Intent acHomeCale = new Intent(this, calendario.class);
+              startActivity(acHomeCale);
 
+              break;
 
+          case R.id.btnBuscarEvento:
 
+             //Toast.makeText(this, "BuscarEvento", Toast.LENGTH_SHORT).show();
 
-    //------------------- METODOS PARA CAMBIAR DE ACTIVITY -----------------------//
+              AlertDialog.Builder builder = new AlertDialog.Builder(this);
+              LayoutInflater inflater = getLayoutInflater();
+              View dialogo = inflater.inflate(R.layout.dialogo_buscar_evento, null);
+              builder.setView(dialogo);
+              Button btnBuscar   = (Button) dialogo.findViewById(R.id.btnDiaBusBuscar);
+              //Button btnCancelar = (Button) dialogo.findViewById(R.id.btnDiaBusCancelar);
 
-    //Metodo que redirige al usuario dependiendo el boton oprimido
-    public void intentHomeCalendario(View view){
-        Intent acHomeCale = new Intent(this, calendario.class);
-        startActivity(acHomeCale);
+              AlertDialog ventanaBuscar = builder.create();
+              ventanaBuscar.show();
+              break;
+
+          case R.id.btnEventoInsc:
+
+              Toast.makeText(this, "eventoIns", Toast.LENGTH_SHORT).show();
+
+              break;
+
+          //Los case posteriores hacen referencia a los botones de loa cuadros de dialogo.
+
+          case R.id.btnDiaBusBuscar:
+
+              Toast.makeText(this, "Buscar evento", Toast.LENGTH_SHORT).show();
+              /** HAY QUE CERRAR EL DIALOGO, LUEGO DE HACER EL TOAST */
+
+              break;
+      }
     }
+
 
     //------------------- PARTE GRAFICA -----------------------//
 
@@ -46,4 +82,8 @@ public class guiaUsc extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }*/
+
+    //----------------- PRUEBAS ---------------------//
+
+
 }
